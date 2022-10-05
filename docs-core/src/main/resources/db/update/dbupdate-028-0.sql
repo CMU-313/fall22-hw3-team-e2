d2424f5e730d119edc890989dd4296bb343a3fd6
Zack Zhao <zz0418@DESKTOP-0JI31E3.localdomain>
2022-10-05 13:58:16 -0400

    changed cfg_value to 28 in sql file and changed db version in all config.properties files

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,2 +1,2 @@
 alter table T_DOCUMENT add column DOC_PROGRESS_C enum(1, 2, 3) DEFAULT (1);
-update T_CONFIG set CFG_VALUE_C = '29' where CFG_ID_C = 'DB_VERSION';
+update T_CONFIG set CFG_VALUE_C = '28' where CFG_ID_C = 'DB_VERSION';
